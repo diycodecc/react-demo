e1e04941b40abc446b1949c21001d9d760323f07
CoffeeDeveloper <coffeedeveloper@gmail.com>
2015-02-21 20:04:49 +0800

    update CommentForm

--- js/index.jsx
+++ js/index.jsx
@@ -3,6 +3,7 @@ var CommentBox = React.createClass({
     $.ajax({
       url: this.props.url,
       dataType: 'json',
+      cache: false,
       success: function (data) {
         this.setState({data: data})
       }.bind(this),
@@ -63,11 +64,23 @@ var Comment = React.createClass({
 })
 
 var CommentForm = React.createClass({
+  handleSubmit: function (e) {
+    e.preventDefault()
+    var author = this.refs.author.getDOMNode().value.trim()
+    var text = this.refs.text.getDOMNode().value.trim()
+    if (!text || !author) {
+      return
+    }
+    this.refs.author.getDOMNode().value = ''
+    this.refs.text.getDOMNode().value = ''
+  },
   render: function () {
     return (
-      <div className="commentForm">
-        Hello, world! I am a CommentForm.
-      </div>
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Your name" ref="author" />
+        <input type="text" placeholder="Say something" ref="text" />
+        <input type="submit" value="Post" />
+      </form>
     )
   }
 })
